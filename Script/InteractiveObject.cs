2ef05b5fe11ffa1adbaf0b8e6328a55acfc0e334
LinDi_ <118484191+gzldsss@users.noreply.github.com>
2023-11-24 16:27:28 +0000

    Add files via upload

---
+++ Script/InteractiveObject.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChangeMaterialOnCollision : MonoBehaviour
+{
+    public Material newMaterial; // 新材质
+
+    private Renderer objRenderer;
+    private Material originalMaterial; // 保存原始材质
+
+    void Start()
+    {
+        objRenderer = GetComponent<Renderer>();
+        originalMaterial = objRenderer.material; // 保存原始材质
+    }
+
+    void OnCollisionEnter(Collision collision)
+    {
+        Debug.Log("检测到碰撞: " + collision.gameObject.name); // 输出碰撞的物体名称
+
+        if (collision.gameObject.CompareTag("Player")) // 检查碰撞的是否为玩家
+        {
+            objRenderer.material = newMaterial; // 更换为新材质
+        }
+    }
+}
